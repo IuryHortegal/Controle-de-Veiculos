a74dfa553aebf9c06c38888c414b1f6dc64c1110
Iury Hortegal <iury100hortegal@gmail.com>
2026-09-17 12:51:04 +0000

    Adicionar lista de veículos

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Veiculo veiculo = new Veiculo(
-            "ABC1D23",
-            "Fiat Argo",
-            45000
-        );
+        ArrayList<Veiculo> veiculos = new ArrayList<>();
 
-        System.out.println("Dados iniciais:");
-        veiculo.exibirDados();
+        veiculos.add(new Veiculo("ABC1D23", "Fiat Argo", 45000));
+        veiculos.add(new Veiculo("DEF4G56", "Toyota Corolla", 62000));
+        veiculos.add(new Veiculo("HIJ7K89", "Volkswagen Polo", 28000));
 
-        System.out.println("\nApós atualização:");
-        veiculo.atualizarQuilometragem(52000);
-        veiculo.exibirDados();
+        System.out.println("VEÍCULOS CADASTRADOS");
+        System.out.println();
+
+        for (Veiculo veiculo : veiculos) {
+            veiculo.exibirDados();
+            System.out.println("--------------------");
+        }
     }
 }
